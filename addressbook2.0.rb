4d496b9967960e487d74b4c9cc5d60897ccac0f6
Oscar Jurado <oscjurado@gmail.com>
2014-11-14 14:23:12 -0500

    Moved methods around

---
+++ addressbook2.0.rb
@@ -0,0 +1,38 @@
+require_relative 'lib/connect.rb'
+require_relative 'lib/models.rb'	
+
+
+select_message = 
+"\nSelect an entry from the above list " 
+
+def menu
+	puts "Select an option: \n
+	1. Create new contact \n
+	2. View contact \n
+	3. Delete contact \n
+	4. Quit \n"
+	return gets.chomp
+end
+
+
+def print_list(collection)
+	collection.each_with_index { |entry, index| puts ("#{index} ") + "#{entry.first_name} " + "#{entry.last_name}"}
+end
+
+
+create = "1"
+search = "2"
+quit = "3"
+
+selection = menu
+until selection == quit do 
+	if selection == create
+		entry = create()
+		entry_collection.push entry
+		entry_collection.sort_by! { |n| n.last_name.downcase }	
+	else selection == view
+		search()
+	end
+
+	selection = menu
+end
--- lib/models/addressentry.rb
+++ lib/models/addressentry.rb
@@ -6,4 +6,32 @@ class Addressentry < ActiveRecord::Base
 	has_many :phonenumbers
 	has_many :emails
 
+def create
+	puts "Please enter first name: "
+	fname = gets.chomp.to_s
+
+  puts "Please enter last name: "
+ 	lname = gets.chomp.to_s 
+
+ 	entry = Addressentry.create({first_name: fname, last_name: lname})
+
+
+  puts "Please enter phone category: "
+  	cat = gets.chomp
+  puts "Please enter phone number: "
+  	dig = gets.chomp
+
+  phone = Phonenumber.create({category: cat, digits: dig})
+  	entry.phonenumbers.push phone
+  
+  puts "Please enter email category:"
+  	cat = gets.chomp
+  puts "Please enter email: "
+  	ad = gets.chomp
+  email = Email.new({category: cat, address: ad})
+  	entry.emails.push email
+  
+  return entry
+end
+
 end
